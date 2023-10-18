961f6675a9878a582a595d86385dc9e5efa9abc5
David Okaliwe <okaliwedavid@gmail.com>
2023-10-18 17:26:59 +0100

    Updates

---
+++ constants/index.js
@@ -0,0 +1,23 @@
+export const navLinks = [
+    {
+      id: "about",
+      title: "About the club",
+      route: "/about",
+    },
+    {
+      id: "membership",
+      title: "Membership",
+      route: "/membership",
+    },
+    {
+      id: "events",
+      title: "Events",
+      route: "/events",
+    },
+    {
+      id: "contact",
+      title: "Contact Us",
+      route: "/contact",
+    },
+  ];
+  
