ace64b9b5421bf321732d92170f381fb7a27509b
David Okaliwe <okaliwedavid@gmail.com>
2023-10-21 20:48:55 +0100

    updates

--- components/about.jsx
+++ components/about.jsx
@@ -1,13 +1,20 @@
 import SectionWrapper from "@hoc/SectionWrapper";
+import { motion } from 'framer-motion';
+import { slideIn, textVariant } from '@utils/motion';
 
 const About = () => {
   return (
     <section className="md:min-h-[700px] ss:min-h-[700px] min-h-[700px] 
     mx-auto flex items-center">
-        <div>
-            <div>
-
-            </div>
+        <div className='items-center justify-between w-full mx-auto flex 
+        md:flex-row ss:flex-row flex-col items-start md:gap-5 ss:gap-5 
+        gap-10'>
+            <motion.div variants={slideIn('up', 'tween', 0.2, 0.5)}
+            className="flex flex-row">
+                <h1>
+                    What is Nuude?
+                </h1>
+            </motion.div>
             
             <div>
                 <div>
@@ -15,7 +22,7 @@ const About = () => {
                 </div>
 
                 <div>
-                    
+
                 </div>
             </div>
         </div>
