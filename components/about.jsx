abcd90970f456f24249b549489a977cd291c022b
David Okaliwe <okaliwedavid@gmail.com>
2023-10-21 20:22:30 +0100

    updates

--- components/about.jsx
+++ components/about.jsx
@@ -3,7 +3,7 @@ import SectionWrapper from "@hoc/SectionWrapper";
 const About = () => {
   return (
     <section className="md:min-h-[700px] ss:min-h-[700px] min-h-[700px] 
-    mx-auto flex items-center about">
+    mx-auto flex items-center">
         Hello
     </section>
   )
