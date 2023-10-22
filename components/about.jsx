2baee849ce62c4fca9178a1fce3f4d783e8e5a52
David Okaliwe <okaliwedavid@gmail.com>
2023-10-22 19:46:45 +0100

    updates

--- components/about.jsx
+++ components/about.jsx
@@ -1,4 +1,4 @@
-
+'use client';
 
 import SectionWrapper from "@hoc/SectionWrapper";
 import { motion } from 'framer-motion';
@@ -26,11 +26,11 @@ const About = () => {
             
             <div>
                 <div>
-                    
+
                 </div>
-                    
+
                 <div>
-                    
+
                 </div>
             </div>
         </div>
