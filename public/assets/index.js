e636709c2a3010cf4616cdcb3c378b2c6d503d80
David Okaliwe <okaliwedavid@gmail.com>
2023-10-22 21:24:48 +0100

    updates

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,9 +1,11 @@
 import logo from './logo.png';
 import logoalt from './logoalt.png';
 import heroImg from './heroImg.png';
+import aboutimg from './aboutimg.png';
 
 export {
     logo,
     logoalt,
     heroImg,
+    aboutimg
 }
