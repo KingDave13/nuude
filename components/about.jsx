034a49263bb8b8d805476f158b93ed54b19cdc4c
David Okaliwe <okaliwedavid@gmail.com>
2023-10-22 18:50:36 +0100

    updates

--- components/about.jsx
+++ components/about.jsx
@@ -14,12 +14,12 @@ const About = () => {
             <motion.div variants={slideIn('up', 'tween', 0.2, 0.5)}
             className="flex flex-row items-center justify-between w-full">
                 <h1 className="text-secondary font-bold md:text-[55px]
-                ss:text-[47px] text-[35px]">
+                ss:text-[47px] text-[35px] gap-5">
                     What is Nuude!?
                 </h1>
                 <motion.div variants={slideIn('right', 'tween', 0.2, 0.5)}
                 className="flex relative items-center justify-center">
-                    <div className='bg-secondary md:w-[900px] ss:w-[300px]
+                    <div className='bg-secondary md:w-[1000px] ss:w-[300px]
                         w-[80px] md:h-[1px] ss:h-[1px] h-[2px]' />
                 </motion.div>
             </motion.div>
