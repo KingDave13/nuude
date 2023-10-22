a75efc8daf3713f9446eadaa0e6e9cfb8e010740
David Okaliwe <okaliwedavid@gmail.com>
2023-10-22 20:59:17 +0100

    updates

--- components/about.jsx
+++ components/about.jsx
@@ -8,18 +8,17 @@ const About = () => {
   return (
     <section className="md:min-h-[700px] ss:min-h-[700px] min-h-[700px] 
     mx-auto flex items-center">
-        <div className='items-center w-full mx-auto flex
-        md:flex-row ss:flex-row flex-col items-start md:gap-5 ss:gap-5 
-        gap-10'>
+        <div className='items-center w-full mx-auto flex md:flex-row 
+        ss:flex-row flex-col'>
             <motion.div variants={slideIn('up', 'tween', 0.2, 0.5)}
-            className="flex flex-row items-center justify-between w-full">
+            className="flex flex-row items-center w-full">
                 <h1 className="text-secondary font-bold md:text-[55px]
-                ss:text-[47px] text-[35px] gap-5">
+                ss:text-[47px] text-[35px] md:mr-10">
                     What is Nuude!?
                 </h1>
                 <motion.div variants={slideIn('right', 'tween', 0.2, 0.5)}
-                className="flex relative items-center justify-center">
-                    <div className='bg-secondary md:w-[1000px] ss:w-[300px]
+                className="flex-1 relative items-center justify-center">
+                    <div className='bg-secondary md:w-full ss:w-[300px]
                         w-[80px] md:h-[1px] ss:h-[1px] h-[2px]' />
                 </motion.div>
             </motion.div>
