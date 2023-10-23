c41d425f0dac4664b0779b43d2c2eb06db986f4c
David Okaliwe <okaliwedavid@gmail.com>
2023-10-23 15:51:36 +0100

    updates

---
+++ components/Events.jsx
@@ -0,0 +1,10 @@
+import SectionWrapper from '@hoc/SectionWrapper'
+import React from 'react'
+
+const Events = () => {
+  return (
+    <div>Events</div>
+  )
+}
+
+export default SectionWrapper(Events, 'events');
