8410cd0aedeb3f237638c1c690b69e7983553b82
David Okaliwe <okaliwedavid@gmail.com>
2023-10-23 18:05:11 +0100

    updates

--- components/Membership.jsx
+++ components/Membership.jsx
@@ -8,69 +8,67 @@ const Membership = () => {
   return (
     <section className="md:min-h-[700px] ss:min-h-[700px] min-h-[700px] 
     mx-auto flex items-center">
-        <div className='items-center w-full mx-auto flex flex-col'>
+        <div className='items-center w-full mx-auto flex flex-col md:pb-10'>
             <motion.div variants={slideIn('up', 'tween', 0.2, 0.5)}
-            className="flex flex-row items-center w-full md:mb-14">
+            className="flex flex-col items-center w-full md:mb-10">
                 <h1 className="text-white font-bold md:text-[55px]
-                ss:text-[47px] text-[35px] md:mr-14">
+                ss:text-[47px] text-[35px]">
                     Membership
                 </h1>
                 <motion.div variants={slideIn('right', 'tween', 0.2, 0.5)}
-                className="flex-1 relative items-center justify-center">
+                className="flex-wrap relative w-full bottom-[30px]">
                     <div className='bg-white md:w-full ss:w-[300px]
                         w-[80px] md:h-[1px] ss:h-[1px] h-[2px]' />
                 </motion.div>
             </motion.div>
-            
-            <motion.div variants={fadeIn('left', 'spring', 0.3)}
-            className="flex flex-col w-full">
-                <motion.div variants={textVariant()}>
-                    <p className='text-white md:text-[17px] ss:text-[18px] 
-                    text-[14px]'>
-                        Important Information Before Applying
-                    </p>
 
-                    <p className='text-white md:text-[17px] ss:text-[18px] 
-                    text-[14px] max-w-[700px] md:mt-5 md:leading-[22px]
-                    text-center'>
-                    To gain entry into Nuude!, one must apply. An 
-                    Aurum-level annual membership, at minimum, 
-                    is required for any single gentleman to attend a 
-                    Nuude! event. Approved non-member couples or singles 
-                    who have never attended will have the opportunity to 
-                    join us for dinner.
-                    </p>
+            <motion.div variants={textVariant()}
+            className='flex-col justify-center items-center'>
+                <p className='text-white md:text-[17px] ss:text-[18px] 
+                text-[14px] text-center'>
+                    Important Information Before Applying
+                </p>
 
-                    <p className='text-white md:text-[17px] ss:text-[18px] 
-                    text-[14px] max-w-[700px] md:mt-5 md:leading-[22px]
-                    text-center'>
-                    This is a way for us to get to know our guests, and 
-                    also to ensure every Nuude! event is a singular 
-                    evening with a carefully curated crowd. We screen 
-                    for aesthetic appeal, professional status and what 
-                    one will contribute to the Nuude! community.
-                    </p>
+                <p className='text-white md:text-[17px] ss:text-[18px] 
+                text-[14px] md:max-w-[850px] md:mt-5 md:leading-[22px]
+                text-center'>
+                To gain entry into Nuude!, one must apply. An 
+                Aurum-level annual membership, at minimum, 
+                is required for any single gentleman to attend a 
+                Nuude! event. Approved non-member couples or singles 
+                who have never attended will have the opportunity to 
+                join us for dinner.
+                </p>
 
-                    <p className='text-white md:text-[17px] ss:text-[18px] 
-                    text-[14px] max-w-[700px] md:mt-5 md:leading-[22px]
-                    text-center'>
-                    All submissions are confidential and reviewed solely 
-                    by our Council for the purpose of maintaining the 
-                    highest calibre of guests at our events. If an 
-                    applicant meets Nuude!'s professional, reputational, 
-                    and aesthetic criteria the applicant will be granted 
-                    the status of Approved Non-Member and contacted 
-                    directly.
-                    </p>
+                <p className='text-white md:text-[17px] ss:text-[18px] 
+                text-[14px] md:max-w-[850px] md:mt-5 md:leading-[22px]
+                text-center'>
+                This is a way for us to get to know our guests, and 
+                also to ensure every Nuude! event is a singular 
+                evening with a carefully curated crowd. We screen 
+                for aesthetic appeal, professional status and what 
+                one will contribute to the Nuude! community.
+                </p>
 
-                    <p className='text-white md:text-[17px] ss:text-[18px] 
-                    text-[14px] max-w-[700px] md:mt-5 md:leading-[22px]
-                    text-center'>
-                    Applicant's requiring the greatest level of discretion 
-                    may forego our application and be interviewed directly 
-                    by Nuude!.
-                    </p>    
-                </motion.div>
+                <p className='text-white md:text-[17px] ss:text-[18px] 
+                text-[14px] md:max-w-[850px] md:mt-5 md:leading-[22px]
+                text-center'>
+                All submissions are confidential and reviewed solely 
+                by our Council for the purpose of maintaining the 
+                highest calibre of guests at our events. If an 
+                applicant meets Nuude!'s professional, reputational, 
+                and aesthetic criteria the applicant will be granted 
+                the status of Approved Non-Member and contacted 
+                directly.
+                </p>
+
+                <p className='text-white md:text-[17px] ss:text-[18px] 
+                text-[14px] md:max-w-[850px] md:mt-5 md:leading-[22px]
+                text-center'>
+                Applicant's requiring the greatest level of discretion 
+                may forego our application and be interviewed directly 
+                by Nuude!.
+                </p>    
             </motion.div>
         </div>
     </section>
