4ee9a17b537f83504f01606a2ff348a1c35ef89f
David Okaliwe <okaliwedavid@gmail.com>
2023-10-24 23:05:55 +0100

    updates

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -3,7 +3,6 @@
 import SectionWrapper from "@hoc/SectionWrapper";
 import { motion } from 'framer-motion';
 import { slideIn, textVariant } from '@utils/motion';
-import { layout } from "@styles/styles";
 
 const Contact = () => {
   return (
@@ -60,8 +59,8 @@ const Contact = () => {
                     </div>
 
                     <div className="flex flex-col">
-                        <label className="text-white md:mb-4 ss:mb-2 mb-2 
-                        md:text-[20px] ss:text-[18px] text-[14px]">
+                        <label className="text-white md:mb-3 ss:mb-2 mb-2 
+                        md:text-[17px] ss:text-[18px] text-[14px]">
                         Last Name
                         </label>
                         <input
@@ -70,10 +69,12 @@ const Contact = () => {
                         // value={form.name}
                         // onChange={handleChange}
                         placeholder="Enter your last name"
-                        className="md:py-3 ss:py-3 py-2 px-6 border-[1px] 
-                        outline-none text-white rounded-lg placeholder:text-black-100 
-                        md:placeholder:text-[15px] ss:placeholder:text-[14px] 
-                        placeholder:text-[12px]"
+                        className="md:py-4 ss:py-3 py-2 px-4 border-none 
+                        outline-none text-white md:rounded-[3px] 
+                        placeholder:text-white-4 font-manier
+                        md:placeholder:text-[15px] 
+                        ss:placeholder:text-[14px] 
+                        placeholder:text-[12px] bg-primaryalt"
                         />
                     </div>
 
