a5ddff5414f13d9e8b0e89fdc21b50454ec2cd54
David Okaliwe <okaliwedavid@gmail.com>
2023-10-24 22:28:56 +0100

    updates

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -22,52 +22,16 @@ const Contact = () => {
                         w-[80px] md:h-[1px] ss:h-[1px] h-[2px]' />
                 </motion.div>
             </motion.div>
-            
-            <motion.div variants={slideIn('down', 'tween', 0.2, 0.5)}
-            className="flex md:flex-row ss:flex-row flex-col gap-10 w-full">
-                <motion.div variants={textVariant()}
-                className={`${layout.sectionInfo}`}>
-                    <p className='text-white md:text-[17px] ss:text-[18px] 
-                    text-[14px] max-w-[700px] md:leading-[22px]'>
-                    Got any more inquiries, complaints, sponsorship 
-                    proposals, payment disputes and any other general 
-                    info? Contact us today and we'll get back to you 
-                    very, very swiftly.
-                    </p>
-
-                    <p className='text-white md:text-[17px] ss:text-[18px] 
-                    text-[14px] max-w-[700px] md:mt-5 md:leading-[22px]'>
-                    In addition, the pool parties add an exciting and 
-                    playful dimension to the luxury erotic experience. 
-                    These poolside events offer a relaxed yet sensuous 
-                    atmosphere, where guests can enjoy the company of 
-                    like-minded individuals in a more casual setting. 
-                    The Pool Party series often features entertainment, 
-                    music, and activities that embrace the spirit of 
-                    sensuality and freedom.
-                    </p>    
-
-                    <div className='md:mt-16 ss:mt-8 mt-4 flex md:gap-5'>
-                        <button
-                            className='grow4 bg-secondary
-                            md:text-[17px] ss:text-[17px] text-[14px] md:py-4
-                            ss:py-3 py-2 md:px-10 ss:px-14 px-6 text-primary 
-                            rounded-[6px] font-medium font-manier cursor-pointer'
-                        >
-                            Become a Member
-                        </button>
-
-                        <button
-                            className='border-[1px] grow2 border-secondary 
-                            md:text-[17px] ss:text-[17px] text-[14px] md:py-4 
-                            ss:py-3 py-2 md:px-16 ss:px-14 px-6 text-secondary 
-                            rounded-[6px] font-medium bg-transparent font-manier
-                            cursor-pointer'
-                        >
-                            View Events
-                        </button>
-                    </div>
-                </motion.div>
+        
+            <motion.div variants={textVariant()}
+            className={`${layout.sectionInfo}`}>
+                <p className='text-white md:text-[17px] ss:text-[18px] 
+                text-[14px] max-w-[700px] md:leading-[22px]'>
+                Got any more inquiries, complaints, sponsorship 
+                proposals, payment disputes and any other general 
+                info? Contact us today and we'll get back to you 
+                very, very swiftly.
+                </p>
             </motion.div>
         </div>
     </section>
