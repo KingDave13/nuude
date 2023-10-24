67454710346d714bd6edbe76bfc604bcf166ef20
David Okaliwe <okaliwedavid@gmail.com>
2023-10-24 22:34:48 +0100

    updates

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -33,6 +33,114 @@ const Contact = () => {
                 very, very swiftly.
                 </p>
             </motion.div>
+
+            <motion.div variants={slideIn('down', 'tween', 0.2, 1)}
+                className='flex-1 bg-primary bg-opacity-40 p-8 md:mt-0 ss:mt-5
+                mt-0 items-start'>
+                <form ref={formRef} onSubmit={handleSubmit}
+                className="grid grid-cols-2 gap-8">
+                    <div className="flex flex-col">
+                        <label className="text-primary font-bold md:mb-4 ss:mb-2 mb-2 
+                        md:text-[20px] ss:text-[18px] text-[14px]">
+                        Name
+                        </label>
+                        <input
+                        type="text"
+                        name="name"
+                        value={form.name}
+                        onChange={handleChange}
+                        placeholder="Enter your name"
+                        className="md:py-3 ss:py-3 py-2 px-6 border-[1px] 
+                        outline-none text-black rounded-lg placeholder:text-black-100 
+                        md:placeholder:text-[15px] ss:placeholder:text-[14px] 
+                        placeholder:text-[12px]"
+                        />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className="text-primary font-bold md:mb-4 ss:mb-2 mb-2 
+                        md:text-[20px] ss:text-[18px] text-[14px]">
+                        Email
+                        </label>
+                        <input
+                        type="email"
+                        name="email"
+                        value={form.email}
+                        onChange={handleChange}
+                        placeholder="Enter your email"
+                        className="md:py-3 ss:py-3 py-2 px-6 border-[1px] 
+                        outline-none text-black rounded-lg placeholder:text-black-100 
+                        md:placeholder:text-[15px] ss:placeholder:text-[14px] 
+                        placeholder:text-[12px]"
+                        />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className="text-primary font-bold md:mb-4 ss:mb-2 mb-2 
+                        md:text-[20px] ss:text-[18px] text-[14px]">
+                        Phone Number
+                        </label>
+                        <input
+                        type="text"
+                        name="phone"
+                        value={form.phone}
+                        onChange={handleChange}
+                        placeholder="Enter your phone(WhatsApp) number"
+                        className="md:py-3 ss:py-3 py-2 px-6 border-[1px] 
+                        outline-none text-black rounded-lg placeholder:text-black-100 
+                        md:placeholder:text-[15px] ss:placeholder:text-[14px] 
+                        placeholder:text-[12px]"
+                        />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className="text-primary font-bold md:mb-4 ss:mb-2 mb-2 
+                        md:text-[20px] ss:text-[18px] text-[14px]">
+                        Subject
+                        </label>
+                        <input
+                        type="text"
+                        name="subject"
+                        value={form.subject}
+                        onChange={handleChange}
+                        placeholder="Enter a subject"
+                        className="md:py-3 ss:py-3 py-2 px-6 border-[1px] 
+                        outline-none text-black rounded-lg placeholder:text-black-100 
+                        md:placeholder:text-[15px] ss:placeholder:text-[14px] 
+                        placeholder:text-[12px]"
+                        />
+                    </div>
+
+                    <div className="col-span-2 flex flex-col">
+                        <label className="text-primary font-bold md:mb-4 ss:mb-2 mb-2 
+                        md:text-[20px] ss:text-[18px] text-[14px]">
+                        Message
+                        </label>
+                        <textarea
+                        rows="5"
+                        name="message"
+                        value={form.message}
+                        onChange={handleChange}
+                        placeholder="Your message"
+                        className="md:py-3 ss:py-3 py-2 px-6 border-[1px] 
+                        outline-none text-black rounded-lg placeholder:text-black-100 
+                        md:placeholder:text-[15px] ss:placeholder:text-[14px] 
+                        placeholder:text-[12px]"
+                        />
+                    </div>
+
+                    <div className="col-span-2">
+                        <button
+                        type="submit"
+                        className="bg-primary grow2 mr-5 w-fit shadow-md 
+                        md:text-[16px] ss:text-[16px] text-[13px] py-2 px-12 
+                        text-white rounded-lg font-medium border-none hover:text-white"
+                        >
+                        {Loading ? 'Sending...' : 'Send'}
+                        </button>
+                    </div>
+                </form>
+            </motion.div>
         </div>
     </section>
   )
