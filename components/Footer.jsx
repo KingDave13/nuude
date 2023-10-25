5d884777b8dcc034200aaa6050539c92a2b8d9fd
David Okaliwe <okaliwedavid@gmail.com>
2023-10-25 11:56:03 +0100

    updates

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -26,10 +26,9 @@ const Footer = () => {
                 <p className='text-maintext md:text-[18px] ss:text-[16px] 
                 text-[14px] md:max-w-[550px] ss:max-w-[260px] max-w-[320px] 
                 md:mt-8 ss:mt-8 mt-5 md:leading-[25px] ss:leading-[23px] 
-                leading-[20px]'>
-                Anayo Okpala Global Concept is a leading supplier of 
-                high-quality foam cloth, tape edge, PVC, sewing thread, 
-                cellotape and other foam accessories for various industries.
+                leading-[20px] text-center'>
+                    Nuude! is an exclusively private social and 
+                    entertainment club in Lagos, Nigeria.
                 </p>
             </motion.div>
 
@@ -37,7 +36,7 @@ const Footer = () => {
             className='flex'>
                 <div className='flex md:mt-3 ss:mt-2 mt-1 md:mb-8 ss:mb-0 mb-12 
                 items-center'>
-                    <BiCopyright className='sm:mr-2 mr-1 md:text-[20px] 
+                    <BiCopyright className='sm:mr-1 mr-1 md:text-[16px] 
                     ss:text-[18px] text-[15px] md:mt-1 ss:mt-1 mt-[3px]
                     text-maintext' />
 
