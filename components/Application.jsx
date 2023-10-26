4e6818eb26e403145baaf087948f4e2f10f1f60f
David Okaliwe <okaliwedavid@gmail.com>
2023-10-26 21:17:24 +0100

    updates

--- components/Application.jsx
+++ components/Application.jsx
@@ -31,7 +31,122 @@ const Application = () => {
 
             <motion.div variants={slideIn('down', 'tween', 0.2, 1)}
             className='flex md:flex-row w-full'>
+                <div className='bg-primary p-12 md:mt-0 ss:mt-5 mt-0
+                w-1/2'>
+                    <form className="grid grid-cols-2 gap-8">
+                        <div className="flex flex-col">
+                            <label className="text-white md:mb-3 ss:mb-2 mb-2 
+                            md:text-[16px] ss:text-[18px] text-[14px]">
+                            First Name
+                            </label>
+                            <input
+                            type="text"
+                            name="firstname"
+                            // value={form.name}
+                            // onChange={handleChange}
+                            placeholder="Enter your first name"
+                            className="md:py-3 ss:py-3 py-2 px-4 border-none 
+                            outline-none text-white md:rounded-[3px] 
+                            placeholder:text-white-4 font-manier
+                            md:placeholder:text-[14px] 
+                            ss:placeholder:text-[14px] 
+                            placeholder:text-[12px] bg-primaryalt"
+                            />
+                        </div>
 
+                        <div className="flex flex-col">
+                            <label className="text-white md:mb-3 ss:mb-2 mb-2 
+                            md:text-[16px] ss:text-[18px] text-[14px]">
+                            Last Name
+                            </label>
+                            <input
+                            type="text"
+                            name="lastname"
+                            // value={form.name}
+                            // onChange={handleChange}
+                            placeholder="Enter your last name"
+                            className="md:py-3 ss:py-3 py-2 px-4 border-none 
+                            outline-none text-white md:rounded-[3px] 
+                            placeholder:text-white-4 font-manier
+                            md:placeholder:text-[14px] 
+                            ss:placeholder:text-[14px] 
+                            placeholder:text-[12px] bg-primaryalt"
+                            />
+                        </div>
+
+                        <div className="col-span-2 flex flex-col">
+                            <label className="text-white md:mb-3 ss:mb-2 mb-2 
+                            md:text-[16px] ss:text-[18px] text-[14px]">
+                            Email Address
+                            </label>
+                            <input
+                            type="email"
+                            name="email"
+                            // value={form.email}
+                            // onChange={handleChange}
+                            placeholder="Enter your email address"
+                            className="md:py-3 ss:py-3 py-2 px-4 border-none 
+                            outline-none text-white md:rounded-[3px] 
+                            placeholder:text-white-4 font-manier
+                            md:placeholder:text-[14px] 
+                            ss:placeholder:text-[14px] 
+                            placeholder:text-[12px] bg-primaryalt"
+                            />
+                        </div>
+
+                        <div className="col-span-2 flex flex-col">
+                            <label className="text-white md:mb-3 ss:mb-2 mb-2 
+                            md:text-[16px] ss:text-[18px] text-[14px]">
+                            Subject
+                            </label>
+                            <input
+                            type="text"
+                            name="subject"
+                            // value={form.subject}
+                            // onChange={handleChange}
+                            placeholder="Enter a subject"
+                            className="md:py-3 ss:py-3 py-2 px-4 border-none 
+                            outline-none text-white md:rounded-[3px] 
+                            placeholder:text-white-4 font-manier
+                            md:placeholder:text-[14px] 
+                            ss:placeholder:text-[14px] 
+                            placeholder:text-[12px] bg-primaryalt"
+                            />
+                        </div>
+
+                        <div className="col-span-2 flex flex-col">
+                            <label className="text-white md:mb-3 ss:mb-2 mb-2 
+                            md:text-[16px] ss:text-[18px] text-[14px]">
+                            Message
+                            </label>
+                            <textarea
+                            rows="6"
+                            name="message"
+                            // value={form.message}
+                            // onChange={handleChange}
+                            placeholder="You can be brief or very expressive. No pressure!"
+                            className="md:py-3 ss:py-3 py-2 px-4 border-none 
+                            outline-none text-white md:rounded-[3px] 
+                            placeholder:text-white-4 font-manier
+                            md:placeholder:text-[14px] 
+                            ss:placeholder:text-[14px] 
+                            placeholder:text-[12px] bg-primaryalt"
+                            />
+                        </div>
+
+                        <div className="col-span-2">
+                            <button
+                            type="submit"
+                            className="bg-secondary grow2 w-fit shadow-md 
+                            md:text-[16px] ss:text-[16px] text-[13px] py-4 px-20
+                            text-primary md:rounded-[3px] border-none
+                            font-manier cursor-pointer"
+                            >
+                            {/* {Loading ? 'Sending...' : 'Send'} */} Submit
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </motion.div>
         </div>
     </section>
