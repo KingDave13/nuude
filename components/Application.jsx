a14e97f0c1c3412de263c0496bd3b013f3956fe3
David Okaliwe <okaliwedavid@gmail.com>
2023-10-26 21:54:40 +0100

    updates

--- components/Application.jsx
+++ components/Application.jsx
@@ -149,12 +149,41 @@ const Application = () => {
 
                 <div className='bg-primaryalt flex flex-col rounded-md
                 md:p-10 w-1/2'>
-                    <p className=''>
+                    <p className='text-white md:text-[16px] text-justify'>
                         By submitting an application for membership or
                         event attendance with Nuude! (referred to as 
                         "Nuude" or "the club" herein), you acknowledge
                         and agree to the following terms and conditions:
                     </p>
+
+                    <ol>
+                        <li>
+
+Application Process: To gain entry into Nuude!, all individuals must apply. An Aurum-level annual membership, at a minimum, is required for single gentlemen to attend any Nuude! event. Approved non-member couples or singles who have not previously attended may be invited to join for Dinner to establish familiarity.
+Membership Screening: Nuude! values the quality of its community and carefully curates its guests. We screen applicants for aesthetic appeal, professional status, and their potential contribution to the Nuude! community. All applications are treated with the utmost confidentiality and reviewed exclusively by our Council to maintain the highest caliber of guests at our events.
+Approval Process: If an applicant meets Nuude!'s professional, reputational, and aesthetic criteria, they may be granted the status of an Approved Non-Member and will be contacted directly by our team.
+Discretion: For individuals requiring the highest level of discretion, Nuude! offers the option to forego the standard application process and be interviewed directly by our team. This ensures your privacy and confidentiality.
+Age and Consent: Applicants must be of legal age to participate in adult-themed events in their respective jurisdiction. By applying for membership or attending Nuude! events, you confirm your legal eligibility and consent to participate in such activities.
+Respect and Conduct: All members and guests are expected to treat each other with respect, dignity, and consent. Any form of harassment, discrimination, or non-consensual behavior will not be tolerated and may result in immediate removal from Nuude! events and potential membership revocation.
+Event Details: Nuude! reserves the right to modify or cancel events, change event locations, or adjust event details as needed. Members will be notified of any changes in advance whenever possible.
+Membership Fees: Membership fees are non-refundable and non-transferable. You are responsible for keeping your membership information up to date.
+Termination of Membership: Nuude! reserves the right to terminate or suspend a member's membership at its discretion for violations of these terms and conditions or for any other reason deemed necessary.
+Liability: You understand and acknowledge that participation in Nuude! events is at your own risk. Nuude! and its affiliates are not responsible for any injuries, damages, or losses that may occur during events.
+
+The privacy and safety of Nuude! and our guests is of paramount importance and confidentiality is required of all members, guests, performers, employees, and officers.
+To participate in any Nuude! Event, you hereby also agree to the following instructions:
+
+(i) You will not disclose identifying information of any Nuude! member, guest, participant, performer, employee, or officer to the media;
+(ii) You will not use or attempt to use photography or video recording at a Nuude! Event;
+(iii) You will treat everyone with respect;
+(iv) You will ask before you touch;
+(v) You will immediately notify Nuude! security or staff if at any time you are aware or have reasonable cause to suspect that anyone's safety or ability to consent is at risk;
+(vi) You acknowledge that any violation of this agreement will result in immediate removal from the event, without refund, and a permanent ban from all future events.
+(vil) There are no refunds.
+
+By submitting an application, you affirm that you have read, understood, and agree to abide by these terms and conditions. Nuude! reserves the right to update or modify these terms and conditions at any time, and it is your responsibility to review them periodically.
+                        </li>
+                    </ol>
                 </div>
             </motion.div>
         </div>
