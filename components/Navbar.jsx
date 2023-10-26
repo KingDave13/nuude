fd713259b0118cd0d0d0b2f525f7686ef6474cf8
David Okaliwe <okaliwedavid@gmail.com>
2023-10-26 20:43:02 +0100

    updates

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`w-full flex items-center fixed md:py-5 ss:py-6 
+      className={`w-full flex items-center fixed md:py-6 ss:py-6 
       py-5 md:px-16 ss:px-16 px-6 top-0 z-20 navsmooth ${
         isScrolled ? 'bg-white shadow-lg' : '' }`}
     >
@@ -62,7 +62,7 @@ const Navbar = () => {
             <Image
               src={isScrolled ? logo : logoalt}
               alt="logo"
-              width={150}
+              width={160}
               height={'auto'}
               className="object-contain"
             />
