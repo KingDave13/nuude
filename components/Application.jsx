78ffe36b88c6d4a0ffcc86c6d6592c06f00ef574
David Okaliwe <okaliwedavid@gmail.com>
2023-10-28 21:16:44 +0100

    updates

--- components/Application.jsx
+++ components/Application.jsx
@@ -143,7 +143,7 @@ const Application = () => {
                             text-primary md:rounded-[3px] border-none
                             cursor-pointer"
                             >
-                            {/* {Loading ? 'Sending...' : 'Send'} */} Submit
+                                {/* {Loading ? 'Sending...' : 'Send'} */} Submit
                             </button>
                         </div>
                     </form>
