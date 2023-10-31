af690a2150ae7acb5322b9a247e8c8dfa1c40733
David Okaliwe <okaliwedavid@gmail.com>
2023-10-31 18:30:36 +0100

    Updates

--- components/Application.jsx
+++ components/Application.jsx
@@ -127,7 +127,7 @@ const Application = () => {
                             name="birthdate"
                             // value={form.name}
                             // onChange={handleChange}
-                            placeholder="Enter your first name"
+                            placeholder="DD/MM/YYYY"
                             className="md:py-3 ss:py-3 py-2 px-4 border-none 
                             outline-none text-white md:rounded-[3px] 
                             placeholder:text-white-4
@@ -140,21 +140,26 @@ const Application = () => {
                         <div className="flex flex-col">
                             <label className="text-white md:mb-3 ss:mb-2 mb-2 
                             md:text-[16px] ss:text-[18px] text-[14px]">
-                                Last Name
+                                Gender
                             </label>
-                            <input
+                            <select
                             type="text"
-                            name="lastname"
-                            // value={form.name}
+                            name="gender"
+                            // value={form.subject}
                             // onChange={handleChange}
-                            placeholder="Enter your last name"
+                            placeholder="select a gender"
                             className="md:py-3 ss:py-3 py-2 px-4 border-none 
                             outline-none text-white md:rounded-[3px] 
                             placeholder:text-white-4
                             md:placeholder:text-[14px] 
                             ss:placeholder:text-[14px] 
                             placeholder:text-[12px] bg-primaryalt"
-                            />
+                            >
+                                <option>Male</option>
+                                <option>Female</option>
+                                <option>I prefer not to say</option>
+                                <option>Other</option>
+                            </select>
                         </div>
 
                         <div className="col-span-2 flex flex-col">
