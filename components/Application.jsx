f7b167fe15fd3b74773d82f6d004d7d9cb613268
David Okaliwe <okaliwedavid@gmail.com>
2023-11-01 14:15:58 +0100

    Updates

--- components/Application.jsx
+++ components/Application.jsx
@@ -230,7 +230,7 @@ const Application = () => {
                             placeholder="www.twitter.com/your username"
                             className="md:py-3 ss:py-3 py-2 px-4 border-none 
                             outline-none text-white md:rounded-[3px] 
-                            placeholder:text-white-4
+                            placeholder:text-textalt
                             md:placeholder:text-[14px] 
                             ss:placeholder:text-[14px] 
                             placeholder:text-[12px] bg-primaryalt"
@@ -250,7 +250,7 @@ const Application = () => {
                             placeholder="www.facebook.com/your username"
                             className="md:py-3 ss:py-3 py-2 px-4 border-none 
                             outline-none text-white md:rounded-[3px] 
-                            placeholder:text-white-4
+                            placeholder:text-textalt
                             md:placeholder:text-[14px] 
                             ss:placeholder:text-[14px] 
                             placeholder:text-[12px] bg-primaryalt"
@@ -270,7 +270,7 @@ const Application = () => {
                             placeholder="You can be brief or very expressive. No pressure!"
                             className="md:py-3 ss:py-3 py-2 px-4 border-none 
                             outline-none text-white md:rounded-[3px] 
-                            placeholder:text-white-4
+                            placeholder:text-textalt
                             md:placeholder:text-[14px] 
                             ss:placeholder:text-[14px] 
                             placeholder:text-[12px] bg-primaryalt"
@@ -290,7 +290,7 @@ const Application = () => {
                             placeholder="You can be brief or very expressive. No pressure!"
                             className="md:py-3 ss:py-3 py-2 px-4 border-none 
                             outline-none text-white md:rounded-[3px] 
-                            placeholder:text-white-4
+                            placeholder:text-textalt
                             md:placeholder:text-[14px] 
                             ss:placeholder:text-[14px] 
                             placeholder:text-[12px] bg-primaryalt"
@@ -310,7 +310,7 @@ const Application = () => {
                             placeholder="You can be brief or very expressive. No pressure!"
                             className="md:py-3 ss:py-3 py-2 px-4 border-none 
                             outline-none text-white md:rounded-[3px] 
-                            placeholder:text-white-4
+                            placeholder:text-textalt
                             md:placeholder:text-[14px] 
                             ss:placeholder:text-[14px] 
                             placeholder:text-[12px] bg-primaryalt"
