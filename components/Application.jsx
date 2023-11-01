b38add909b88fcc5d4e948e5350109f17a98cf7f
David Okaliwe <okaliwedavid@gmail.com>
2023-11-01 09:57:15 +0100

    Updates

--- components/Application.jsx
+++ components/Application.jsx
@@ -112,7 +112,7 @@ const Application = () => {
                             className="md:py-3 ss:py-3 py-2 px-4 border-none 
                             outline-none text-white md:rounded-[3px] 
                             placeholder:text-white-4
-                            md:placeholder:text-[14px] 
+                            md:placeholder:text-[14px]
                             ss:placeholder:text-[14px] 
                             placeholder:text-[12px] bg-primaryalt"
                             />
