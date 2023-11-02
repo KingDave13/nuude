16d0d599ba45a99521ccb1a452079784c64630a5
David Okaliwe <okaliwedavid@gmail.com>
2023-11-02 14:54:46 +0100

    Updates

--- components/Events.jsx
+++ components/Events.jsx
@@ -13,7 +13,8 @@ const Events = () => {
         <div className='items-center w-full mx-auto flex flex-col 
         font-manierRegular'>
             <motion.div variants={slideIn('up', 'tween', 0.2, 0.5)}
-            className="flex flex-row items-center w-full md:mb-20">
+            className="flex flex-row items-center w-full md:mb-20 
+            ss:mb-10">
                 <h1 className="text-secondary font-manierMedium md:text-[55px]
                 ss:text-[40px] text-[35px] md:mr-14 ss:mr-6">
                     Upcoming Events
@@ -37,56 +38,60 @@ const Events = () => {
                 </motion.div>
 
                 <motion.div variants={textVariant()}>
-                    <h1 className='text-white md:text-[40px] ss:text-[35px] 
+                    <h1 className='text-white md:text-[40px] ss:text-[30px] 
                     text-[20px] font-manierMedium md:mb-5 ss:mb-3'>
                         Nuude!
                     </h1>
 
                     <p className='text-white md:text-[18px] ss:text-[15px] 
                     text-[14px] md:max-w-[700px] md:leading-[22px] 
+                    ss:leading-[20px]
                     md:mb-5 ss:mb-3'>
                     Step into a world of fine dining, intrigue and 
                     sensuality where we hand you the key to your dreams.
                     </p>
 
                     <p className='text-white md:text-[17px] ss:text-[15px] 
-                    text-[14px] md:mb-5 ss:mb-3'>
+                    text-[14px] md:mb-5 ss:mb-5'>
                     Featuring: Connect, pool party, fine dining.
                     </p>
 
                     <div className="relative items-center justify-center
-                    md:mb-5">
-                        <div className='bg-secondary md:w-full ss:w-[300px]
+                    md:mb-5 ss:mb-5">
+                        <div className='bg-secondary md:w-full ss:w-full
                         w-[80px] md:h-[1px] ss:h-[1px] h-[1px]' />
                     </div>
 
-                    <h1 className='text-white md:text-[40px] ss:text-[35px] 
-                    text-[20px] font-manierMedium mb-5'>
+                    <h1 className='text-white md:text-[40px] ss:text-[30px] 
+                    text-[20px] font-manierMedium'>
                         24th November, 2023
                     </h1>
 
-                    <p className='text-white md:text-[16px] ss:text-[16px] 
-                    text-[14px] max-w-[750px] md:mt-5 md:leading-[22px]'>
+                    <p className='text-white md:text-[16px] ss:text-[14px] 
+                    text-[14px] max-w-[750px] md:mt-5 md:leading-[22px]
+                    ss:leading-[20px] ss:mt-3'>
                     Other details including time and venue will be 
                     communicated privately via email after successful 
                     screening and approval.
                     </p>    
 
-                    <div className='md:mt-16 ss:mt-8 mt-4 flex md:gap-5'>
+                    <div className='md:mt-16 ss:mt-8 mt-4 flex md:gap-5
+                    ss:gap-5'>
                         <button
                             className='grow4 bg-secondary border-none
-                            md:text-[17px] ss:text-[17px] text-[14px] md:py-4
-                            ss:py-3 py-2 md:px-10 ss:px-14 px-6 text-primary 
-                            rounded-[6px] cursor-pointer'
+                            md:text-[17px] ss:text-[14px] text-[14px] md:py-4
+                            ss:py-3 py-2 md:px-10 ss:px-5 px-6 text-primary 
+                            md:rounded-[6px] ss:rounded-[3px] cursor-pointer'
                         >
                             Apply for Event
                         </button>
 
                         <button
                             className='border-[1px] grow2 border-secondary 
-                            md:text-[17px] ss:text-[17px] text-[14px] md:py-4 
-                            ss:py-3 py-2 md:px-16 ss:px-14 px-6 text-secondary 
-                            rounded-[6px] bg-transparent cursor-pointer'
+                            md:text-[17px] ss:text-[14px] text-[14px] md:py-4 
+                            ss:py-3 py-2 md:px-16 ss:px-10 px-6 text-secondary 
+                            md:rounded-[6px] ss:rounded-[3px] bg-transparent 
+                            cursor-pointer'
                         >
                             Contact Us
                         </button>
