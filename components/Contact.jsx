cceb61858fc60714a066d66ac1a7e421641bc3f5
David Okaliwe <okaliwedavid@gmail.com>
2023-11-02 15:53:23 +0100

    Updates

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -6,27 +6,28 @@ import { slideIn, textVariant } from '@utils/motion';
 
 const Contact = () => {
   return (
-    <section className="md:min-h-[1100px] ss:min-h-[700px] min-h-[700px] 
+    <section className="md:min-h-[1100px] ss:min-h-[900px] min-h-[700px] 
     mx-auto flex items-center">
         <div className='items-center w-full mx-auto flex flex-col 
         font-manierRegular'>
             <motion.div variants={slideIn('up', 'tween', 0.2, 0.5)}
-            className="flex flex-row items-center w-full md:mb-10">
+            className="flex flex-row items-center w-full md:mb-10 ss:mb-5">
                 <h1 className="text-white font-manierMedium md:text-[55px]
-                ss:text-[47px] text-[35px] md:mr-14">
+                ss:text-[45px] text-[35px] md:mr-14 ss:mr-6">
                     Contact Us
                 </h1>
                 <motion.div variants={slideIn('up', 'tween', 0.2, 0.5)}
                 className="flex-1 relative items-center justify-center">
-                    <div className='bg-white md:w-full ss:w-[300px]
+                    <div className='bg-white md:w-full ss:w-full
                         w-[80px] md:h-[1px] ss:h-[1px] h-[2px]' />
                 </motion.div>
             </motion.div>
         
             <motion.div variants={textVariant()}
-            className='flex items-start w-full md:mb-10'>
-                <p className='text-white md:text-[17px] ss:text-[18px] 
-                text-[14px] md:max-w-[600px] md:leading-[22px]'>
+            className='flex items-start w-full md:mb-10 ss:mb-5'>
+                <p className='text-white md:text-[17px] ss:text-[15px] 
+                text-[14px] md:max-w-[600px] ss:max-w-[600px] 
+                md:leading-[22px]'>
                 Got any more inquiries, complaints, sponsorship 
                 proposals, payment disputes and any other general 
                 info? Contact us today and we'll get back to you 
@@ -36,13 +37,13 @@ const Contact = () => {
 
             <motion.div variants={slideIn('down', 'tween', 0.2, 1)}
             className='w-full'>
-                <div className='bg-primary p-12 md:mt-0 ss:mt-5 mt-0
-                w-1/2'>
+                <div className='bg-primary md:p-12 ss:p-6 md:mt-0 ss:mt-0 
+                mt-0 md:w-1/2'>
                 <form 
-                className="grid grid-cols-2 gap-8">
+                className="grid grid-cols-2 md:gap-8 ss:gap-6">
                     <div className="flex flex-col">
                         <label className="text-white md:mb-3 ss:mb-2 mb-2 
-                        md:text-[16px] ss:text-[18px] text-[14px]">
+                        md:text-[16px] ss:text-[15px] text-[14px]">
                         First Name
                         </label>
                         <input
@@ -52,16 +53,17 @@ const Contact = () => {
                         // onChange={handleChange}
                         placeholder="Enter your first name"
                         className="md:py-3 ss:py-3 py-2 px-4 border-none 
-                        outline-none text-white md:rounded-[3px] 
+                        outline-none text-white md:rounded-[3px]
+                        ss:rounded-[3px] 
                         md:placeholder:text-[14px] 
-                        ss:placeholder:text-[14px] 
+                        ss:placeholder:text-[13px] 
                         placeholder:text-[12px] bg-primaryalt"
                         />
                     </div>
 
                     <div className="flex flex-col">
                         <label className="text-white md:mb-3 ss:mb-2 mb-2 
-                        md:text-[16px] ss:text-[18px] text-[14px]">
+                        md:text-[16px] ss:text-[15px] text-[14px]">
                         Last Name
                         </label>
                         <input
@@ -71,16 +73,17 @@ const Contact = () => {
                         // onChange={handleChange}
                         placeholder="Enter your last name"
                         className="md:py-3 ss:py-3 py-2 px-4 border-none 
-                        outline-none text-white md:rounded-[3px] 
+                        outline-none text-white md:rounded-[3px]
+                        ss:rounded-[3px]
                         md:placeholder:text-[14px] 
-                        ss:placeholder:text-[14px] 
+                        ss:placeholder:text-[13px] 
                         placeholder:text-[12px] bg-primaryalt"
                         />
                     </div>
 
                     <div className="col-span-2 flex flex-col">
                         <label className="text-white md:mb-3 ss:mb-2 mb-2 
-                        md:text-[16px] ss:text-[18px] text-[14px]">
+                        md:text-[16px] ss:text-[15px] text-[14px]">
                         Email Address
                         </label>
                         <input
@@ -90,16 +93,17 @@ const Contact = () => {
                         // onChange={handleChange}
                         placeholder="Enter your email address"
                         className="md:py-3 ss:py-3 py-2 px-4 border-none 
-                        outline-none text-white md:rounded-[3px] 
+                        outline-none text-white md:rounded-[3px]
+                        ss:rounded-[3px]
                         md:placeholder:text-[14px] 
-                        ss:placeholder:text-[14px] 
+                        ss:placeholder:text-[13px] 
                         placeholder:text-[12px] bg-primaryalt"
                         />
                     </div>
 
                     <div className="col-span-2 flex flex-col">
                         <label className="text-white md:mb-3 ss:mb-2 mb-2 
-                        md:text-[16px] ss:text-[18px] text-[14px]">
+                        md:text-[16px] ss:text-[15px] text-[14px]">
                         Subject
                         </label>
                         <input
@@ -109,16 +113,17 @@ const Contact = () => {
                         // onChange={handleChange}
                         placeholder="Enter a subject"
                         className="md:py-3 ss:py-3 py-2 px-4 border-none 
-                        outline-none text-white md:rounded-[3px] 
+                        outline-none text-white md:rounded-[3px]
+                        ss:rounded-[3px]
                         md:placeholder:text-[14px] 
-                        ss:placeholder:text-[14px] 
+                        ss:placeholder:text-[13px] 
                         placeholder:text-[12px] bg-primaryalt"
                         />
                     </div>
 
                     <div className="col-span-2 flex flex-col">
                         <label className="text-white md:mb-3 ss:mb-2 mb-2 
-                        md:text-[16px] ss:text-[18px] text-[14px]">
+                        md:text-[16px] ss:text-[15px] text-[14px]">
                         Message
                         </label>
                         <textarea
@@ -128,9 +133,10 @@ const Contact = () => {
                         // onChange={handleChange}
                         placeholder="You can be brief or very expressive. No pressure!"
                         className="md:py-3 ss:py-3 py-2 px-4 border-none 
-                        outline-none text-white md:rounded-[3px] 
+                        outline-none text-white md:rounded-[3px]
+                        ss:rounded-[3px]
                         md:placeholder:text-[14px] 
-                        ss:placeholder:text-[14px] 
+                        ss:placeholder:text-[13px] 
                         placeholder:text-[12px] bg-primaryalt"
                         />
                     </div>
@@ -139,9 +145,10 @@ const Contact = () => {
                         <button
                         type="submit"
                         className="bg-secondary grow2 w-fit shadow-md 
-                        md:text-[16px] ss:text-[16px] text-[13px] py-4 px-20
-                        text-primary md:rounded-[3px] border-none
-                        cursor-pointer"
+                        md:text-[16px] ss:text-[14px] text-[13px] md:py-4 
+                        ss:py-3 py-0 md:px-20 ss:px-16
+                        text-primary md:rounded-[3px] ss:rounded-[3px] 
+                        border-none cursor-pointer"
                         >
                         {/* {Loading ? 'Sending...' : 'Send'} */} Submit
                         </button>
