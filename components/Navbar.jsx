265af512900a4073bf6c2d266074294d9a98b39d
David Okaliwe <okaliwedavid@gmail.com>
2023-11-08 19:47:40 +0100

    Updates

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -126,7 +126,7 @@ const Navbar = () => {
               <BsX
                 size={63}
                 className="object-contain cursor-pointer bg-white ss:pb-3
-                ss:pt-1 pb-5 pt-3"
+                ss:pt-1 pb-3 pt-3"
                 style={{ color: isScrolled ? '#575656' : '#575656' }}
                 onClick={() => setToggle(!toggle)}
               />
