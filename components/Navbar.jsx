01a589c83523b775064408e26089dd2f1b256eb9
David Okaliwe <okaliwedavid@gmail.com>
2023-11-09 18:04:21 +0100

    Updates

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -107,7 +107,7 @@ const Navbar = () => {
         {/* FOR MOBILE */}
 
         <div className="md:hidden flex justify-between flex-1 items-center
-            mt-3">
+              mt-3">
           <Link href="/" 
             onClick={() => { setActive(''); 
             window.scrollTo(0, 0); }}
