d0435b1db37806cb1583ed575a1ecab5c688a8ce
David Okaliwe <okaliwedavid@gmail.com>
2023-11-11 19:04:28 +0100

    Updates

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -104,7 +104,7 @@ const Navbar = () => {
           </ul>
         </div>
 
-        {/* FOR MOBILEs */}
+        {/* FOR MOBILEss */}
 
         <div className="md:hidden flex justify-between flex-1 items-center
         mt-3">
