c0a2bc11653b50faaf52e0ecf3c041046ffcc378
David Okaliwe <okaliwedavid@gmail.com>
2023-12-02 15:11:01 +0100

    Updates

--- constants/index.js
+++ constants/index.js
@@ -3,10 +3,6 @@ import {
   instagram, 
   facebook, 
   linkedin, 
-  requests,
-  guests,
-  members,
-  announcements,
 } from "@public/assets";
 
 export const navLinks = [
@@ -29,33 +25,6 @@ export const navLinks = [
       id: "contact",
       title: "Contact Us",
       route: "/contact",
-    },
-  ];
-  
-  export const sideLinks = [
-    {
-      id: 'requests',
-      title: 'Requests',
-      route: '/admin/requests',
-      Icon: requests,
-    },
-    {
-      id: 'guests',
-      title: 'Guest List',
-      route: '/admin/guests',
-      Icon: guests,
-    },
-    {
-      id: 'members',
-      title: 'Members',
-      route: '/admin/members',
-      Icon: members,
-    },
-    {
-      id: 'announcements',
-      title: 'Announcements',
-      route: '/admin/announcements',
-      Icon: announcements,
     },
   ];
 
--- public/assets/index.js
+++ public/assets/index.js
@@ -13,11 +13,6 @@ import linkedin from './linkedin.png';
 import arrow from './arrow.png';
 import menu from './menu.png';
 import warning from './warning.png';
-import requests from './requests.png';
-import logout from './logout.png';
-import announcements from './announcements.png';
-import guests from './guests.png';
-import members from './members.png';
 
 export {
     logo,
@@ -35,9 +30,4 @@ export {
     arrow,
     menu,
     warning,
-    requests,
-    logout,
-    announcements,
-    guests,
-    members,
 }
