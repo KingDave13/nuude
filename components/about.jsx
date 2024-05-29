21686bf34a4688f263724b0cd845a287b6b129b3
David Okaliwe <okaliwedavid@gmail.com>
2024-05-29 19:52:57 +0100

    Updates

--- components/about.jsx
+++ components/about.jsx
@@ -2,7 +2,7 @@
 
 import SectionWrapper from "@hoc/SectionWrapper";
 import { motion } from 'framer-motion';
-import { slideIn, textVariant } from '@utils/motion';
+import { fadeIn, textVariant } from '@utils/motion';
 import { aboutimg } from "@public/assets";
 import { layout } from "@styles/styles";
 import Image from "next/image";
@@ -15,20 +15,20 @@ const About = () => {
     mx-auto flex items-center relative">
         <div className='items-center w-full mx-auto flex flex-col 
         font-manierRegular max-w-[95rem] md:mb-0 ss:mb-0'>
-            <motion.div variants={slideIn('up', 'tween', 0.2, 0.5)}
+            <motion.div variants={fadeIn('up', 'tween', 0.2, 0.5)}
             className="flex flex-row items-center w-full md:mb-12 ss:mb-8 
             mb-8">
                 <h1 className="text-secondary font-manierMedium md:text-[55px]
                 ss:text-[45px] text-[33px] md:mr-14 ss:mr-6 mr-5">
                     What is Nuude!?
                 </h1>
-                <motion.div variants={slideIn('down', 'tween', 0.2, 0.5)}
+                <motion.div variants={fadeIn('down', 'tween', 0.2, 0.5)}
                 className="flex-1 relative items-center justify-center">
                     <div className='bg-secondary w-full h-[1px]' />
                 </motion.div>
             </motion.div>
             
-            <motion.div variants={slideIn('down', 'tween', 0.2, 0.5)}
+            <motion.div variants={fadeIn('down', 'tween', 0.2, 0.5)}
             className="flex md:flex-row ss:flex-row flex-col md:gap-10 
             ss:gap-10 gap-12 w-full aboutimage">
                 <motion.div variants={textVariant()}
@@ -57,11 +57,11 @@ const About = () => {
                     and interests.
                     </p>    
 
-                    <div className='flex md:mt-8 ss:mt-8 mt-6 md:gap-8 ss:gap-5 
+                    <div className='flex md:mt-8 ss:mt-8 mt-6 md:gap-6 ss:gap-5 
                     gap-3 items-center font-manierRegular buttonfull'>
                         <button
                             className='grow4 bg-secondary border-none buttonhalf
-                            md:text-[17px] ss:text-[16px] text-[14px] md:py-4
+                            md:text-[17px] ss:text-[16px] text-[14px] md:py-3.5
                             ss:py-3 py-3 md:px-12 ss:px-3 px-3 text-primary 
                             md:rounded-[6px] ss:rounded-[3px] rounded-[3px] 
                             font-medium font-manier cursor-pointer'
@@ -72,7 +72,7 @@ const About = () => {
 
                         <button
                             className='border-[1px] grow2 border-secondary 
-                            md:text-[17px] ss:text-[16px] text-[14px] md:py-4 
+                            md:text-[17px] ss:text-[16px] text-[14px] md:py-3.5 
                             ss:py-3 py-3 md:px-20 ss:px-10 px-6 text-secondary 
                             md:rounded-[6px] ss:rounded-[3px] rounded-[3px] 
                             font-medium bg-transparent font-manier cursor-pointer
@@ -85,7 +85,7 @@ const About = () => {
                 </motion.div>
 
                 <motion.div 
-                    variants={slideIn('down', 'tween', 0.2, 0.5)}>
+                    variants={fadeIn('down', 'tween', 0.2, 0.5)}>
                     <Image src={aboutimg} alt='about'
                     className='md:h-[400px] ss:h-[300px] w-auto'
                     />
