48595a0020e4bc2bc3bd048483f6b1af9e0d4905
David Okaliwe <okaliwedavid@gmail.com>
2024-06-01 12:47:09 +0100

    Updates

--- app/ticket-payment-confirmation-success/page.jsx
+++ app/ticket-payment-confirmation-success/page.jsx
@@ -1,8 +1,6 @@
 'use client';
 
-import SectionWrapper from "@hoc/SectionWrapper";
 import { useRouter } from "next/navigation";
-
 const TicketPaymentSuccess = () => {
 
     const router = useRouter();
@@ -24,26 +22,26 @@ const TicketPaymentSuccess = () => {
             <h1 className='text-secondary md:text-[52px] font-manierMedium
             ss:text-[47px] text-[35px] md:text-center ss:text-center
             md:leading-[20px] ss:leading-[20px] leading-[45px]'>
-                Payment Successful!
+              Payment Successful!
             </h1>
 
             <p className='md:mt-14 ss:mt-12 mt-6 md:text-[18px] ss:text-[18px]
             text-[15px] text-white md:text-center ss:text-center 
             md:leading-[25px] ss:leading-[25px] leading-[22px] 
             md:max-w-[700px] ss:max-w-[700px] max-w-[350px]'>
-             Your payment has been received successfully and thus, validated
-             your application. A payment receipt will be sent to you via the
-             provided email as well as other communications including your
-             ticket number, concise details of the event, marketing
-             info, etc. upon approval.
+              Your payment has been received successfully and thus, validated
+              your application. A payment receipt will be sent to you via the
+              provided email as well as other communications including your
+              ticket number, concise details of the event, marketing
+              info, etc. upon approval.
             </p>
 
             <p className='md:mt-6 ss:mt-6 mt-4 md:text-[18px] ss:text-[18px]
             text-[15px] text-white md:text-center ss:text-center 
             md:leading-[25px] ss:leading-[25px] leading-[22px] 
             md:max-w-[700px] ss:max-w-[700px] max-w-[350px]'>
-             Be sure to check your email regularly- including spam- for
-             information from us regarding your registration for the event.
+              Be sure to check your email regularly- including spam- for
+              information from us regarding your registration for the event.
             </p>
 
             <div className='flex md:mt-8 ss:mt-8 mt-6 md:gap-6 ss:gap-5 
