b5c295e796ae76830a1d567c96aa8f617cad8650
David Okaliwe <okaliwedavid@gmail.com>
2024-06-18 14:08:40 +0100

    Updates

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, options) => {
+    config.experiments = {
+      topLevelAwait: true,
+      layers: true, // Optional, enables 'use client' and 'use server' support
+    };
     config.resolve.alias['@components'] = path.join(__dirname, 'components');
     return config;
   },
